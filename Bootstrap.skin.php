c10b68a07f33e0a384a2f1710b0d6e630ffc5da4
dev.mtyeh411 <dev.mtyeh411@gmail.com>
2012-11-16 14:11:45 -0500

    clean up markup structure; TOC stylings

--- Bootstrap.skin.php
+++ Bootstrap.skin.php
@@ -79,35 +79,37 @@
 							</div>
 						</header>
 					<?php } ?>
-		
-				<div class="container">
-								<div class="row-fluid">
-									<?php $sidebarArticle = Article::newFromTitle(Title::newFromText( $sgSidebarOptions['page']), $this->data['skin']->getContext() );
-												if( $sidebarArticle->getContent() != '' ) { ?>
-													<!-- ===== Sidebar ===== -->
-													<aside class="span3">
-														<?php $renderer->renderSidebar(); ?>
-													</aside>
-									<?php $contentSpanSize = "9"; } ?>
 
-									<!-- ===== Content ===== -->
-									<article class="span<?php echo $contentSpanSize?>" >
-										<div class="page-header">
-											<h1>
-												<?php $this->html( 'title' ) ?>
-												<small><?php $this->html( 'subtitle' ) ?></small>
-											</h1>
-										</div>	
-										<?php $this->html( 'bodycontent' ); ?>
-										<?php $renderer->renderCatLinks(); ?>
-										<?php $this->html( 'dataAfterContent' ); ?>
-									</article>
-								</div> <!-- row -->
-					</div> <!-- container -->
-			</div> <!-- container -->
+				<!-- ===== Sidebar ===== -->
+				<?php $sidebarArticle = Article::newFromTitle(Title::newFromText( $sgSidebarOptions['page']), $this->data['skin']->getContext() );
+					if( $sidebarArticle->getContent() != '' ) { ?>
+						<aside class="span3">
+							<?php $renderer->renderSidebar(); ?>
+						</aside>
+				<?php $contentSpanSize = "9"; } ?>
+	
+				<!-- ===== Article ===== -->	
+				<div class="row-fluid">
+					<!-- ===== Content ===== -->
+					<article class="span<?php echo $contentSpanSize?>" >
+						<div class="page-header">
+							<h1>
+								<?php $this->html( 'title' ) ?>
+								<small><?php $this->html( 'subtitle' ) ?></small>
+							</h1>
+						</div>	
+						<?php $this->html( 'bodycontent' ); ?>
+						<?php $renderer->renderCatLinks(); ?>
+						<?php $this->html( 'dataAfterContent' ); ?>
+					</article>
+				</div>
 
-			<!-- ===== Footer ===== -->
-			<?php $renderer->renderFooter(); ?>
+				<!-- ===== Footer ===== -->
+				<div class="row-fluid">
+					<?php $renderer->renderFooter(); ?>
+				</div>
+
+			</div> <!-- fluid page container -->
 
 			<?php $this->printTrail(); ?>
 
